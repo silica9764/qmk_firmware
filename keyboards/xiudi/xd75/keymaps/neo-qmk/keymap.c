cd86fc938889812c2569cb919ee7db504afa67dc
silica9764 <mhenschel@mailbox.org>
2026-06-07 07:36:25 +0200

    small fix

--- keyboards/xiudi/xd75/keymaps/neo-qmk/keymap.c
+++ keyboards/xiudi/xd75/keymaps/neo-qmk/keymap.c
@@ -67,9 +67,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_L4] = LAYOUT_ortho_5x15(
         KC_ESC,  DE_1,    DE_2,    DE_3,    DE_4,    DE_5,    DE_6,    DE_7,    DE_8,    DE_9,    DE_0,    DE_SS,   DE_ACUT, KC_DEL,  KC_BSPC,
-        KC_TAB,  DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,    DE_Y,    DE_U,    DE_I,    DE_O,    DE_P,    DE_UDIA, KC_VOLD, KC_MUTE, KC_VOLU,
+        KC_TAB,  DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,    DE_Z,    DE_U,    DE_I,    DE_O,    DE_P,    DE_UDIA, KC_VOLD, KC_MUTE, KC_VOLU,
         KC_TRNS, DE_A,    DE_S,    DE_D,    DE_F,    DE_G,    DE_H,    DE_J,    DE_K,    DE_L,    DE_ODIA, DE_ADIA, KC_PGUP, KC_UP,   KC_PGDN,
-        KC_LSFT, DE_Z,    DE_X,    DE_C,    DE_V,    DE_B,    DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_MINS, KC_RSFT, KC_LEFT, KC_DOWN, KC_RGHT,
+        KC_LSFT, DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,    DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_MINS, KC_RSFT, KC_LEFT, KC_DOWN, KC_RGHT,
         KC_LCTL, KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  KC_ENT,  KC_SPC,  KC_ESC,  KC_RCTL, KC_RGUI, KC_RALT, KC_RALT, KC_TRNS, KC_TRNS
     ),
 
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         DE_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, BL_TOGG, QK_BOOT,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, BL_TOGG, QK_BOOT,
         _______, _______, _______, _______, _______, KC_TRNS, _______, _______, _______, _______, _______, _______, _______, KC_TRNS, KC_TRNS
     ),
 
